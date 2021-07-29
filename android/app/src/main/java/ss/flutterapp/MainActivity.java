6ad367e4251faeb71d9c875130973465b325f762
ShivaShirsath <Shiva.S.Shirsath@gmail.com>
2021-07-30 01:35:05 +0530

    Initial commit

---
+++ android/app/src/main/java/ss/flutterapp/MainActivity.java
@@ -0,0 +1,6 @@
+package ss.flutterapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: null,
+        home: Center(child: Text("Hello world !")),
+      )
+  );
+}
